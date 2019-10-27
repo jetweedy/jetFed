fba1cfbd716cd4b2c260c5e9487913e8a08e2f4a
Jon T <jetweedy@gmail.com>
2019-10-27 18:03:10 +0000

    m

---
+++ _test.php
@@ -0,0 +1,7 @@
+<?php
+
+//print "test";
+//print file_get_contents("http://localhost:8380/jetFed/get/test.txt");
+//print file_get_contents("http://localhost:8380/jetFed/_get.php?file=test.txt");
+
+?>
